334dfd3c5c6d252394de2269050b02a61eaf686e
Travis Johnston <tjohnst1@gmail.com>
2016-03-11 09:55:03 -0800

    setup webpack config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './app/js/App.js',
+  output: {
+    path: './dist',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    inline: true,
+    contentBase: './app/dist'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }]
+  }
+
+}
