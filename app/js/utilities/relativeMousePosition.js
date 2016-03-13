48f57b24162ba1e22f3ec6f18b599daf8a1dc889
Travis Johnston <tjohnst1@gmail.com>
2016-03-13 13:32:10 -0700

    install and setup karma for testing

--- app/js/utilities/relativeMousePosition.js
+++ app/js/utilities/relativeMousePosition.js
@@ -1,6 +1,5 @@
 export default function relativeMousePosition(ele, evt){
   const rect = ele.getBoundingClientRect();
-  console.log(rect)
   return {
     x: evt.clientX - rect.left,
     y: evt.clientY - rect.top
