83d07891c963d15810a4ab6bc31ed2d401f96f3b
Travis Johnston <tjohnst1@gmail.com>
2016-03-15 20:45:21 -0700

    create draw function which draws inside the canvas

--- app/js/components/DrawingCanvas.js
+++ app/js/components/DrawingCanvas.js
@@ -3,26 +3,40 @@ import ReactDOM from 'react-dom';
 import relativeMousePosition from '../utilities/relativeMousePosition';
 
 export default class DrawingCanvas extends Component {
-  constructor(){
-    super();
-    this.state = {
-      selectedTool: 'Pencil'
-    }
-  }
 
-  selectTool(toolName){
-    this.setState({selectedTool: toolName})
+  draw(canvas, context){
+    canvas.addEventListener('mousedown', (e) => {
+      let coords = relativeMousePosition(canvas, e);
+      context.beginPath();
+      canvas.addEventListener('mousemove', onMouseMove);
+      canvas.addEventListener('mouseup', onMouseUp);
+
+      function onMouseMove(evt){
+        context.moveTo(coords.x, coords.y);
+        coords = relativeMousePosition(canvas, evt);
+        context.lineTo(coords.x, coords.y);
+        context.stroke();
+      }
+
+      function onMouseUp(evt){
+        canvas.removeEventListener('mousemove', onMouseMove);
+        canvas.removeEventListener('mouseup', onMouseUp);
+        context.closePath();
+      }
+
+    });
   }
 
   componentDidMount(){
     // Find the canvas
     const canvas = ReactDOM.findDOMNode(this.refs.canvas);
     const context = canvas.getContext('2d');
-    canvas.addEventListener('click', (e) => {
-      context.beginPath();
-      const mouseCoords = relativeMousePosition(canvas, e);
-      context.strokeRect(mouseCoords.x, mouseCoords.y, 250, 250);
-    })
+    this.draw(canvas, context);
+    // canvas.addEventListener('click', (e) => {
+    //   context.beginPath();
+    //   const mouseCoords = relativeMousePosition(canvas, e);
+    //   context.strokeRect(mouseCoords.x, mouseCoords.y, 250, 250);
+    // })
   }
 
   render(){
