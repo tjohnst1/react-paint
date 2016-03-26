7ab3c62c6f9337761c52a9a41cfaaa0ed4c2e851
Travis Johnston <tjohnst1@gmail.com>
2016-03-26 12:45:49 -0700

    update tool panel options to account for it appearing only conditionally

--- test/ToolPanelOptions.spec.js
+++ test/ToolPanelOptions.spec.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from 'react';
 import expect from 'expect';
 import TestUtils from 'react-addons-test-utils';
-import ToolPanelOptions from '../app/js/components/ToolPanelOptions';
 import App from '../app/js/container/App'
+import ToolPanel from '../app/js/components/ToolPanel';
+import ToolPanelOptions from '../app/js/components/ToolPanelOptions';
 
-describe('tool panel options component', () => {
+describe('tool panel options', () => {
 
   const app = TestUtils.renderIntoDocument(<App />);
-  const toolPanelOptions = TestUtils.findRenderedComponentWithType(app, ToolPanelOptions);
+  const toolPanel = TestUtils.findRenderedComponentWithType(app, ToolPanel);
+  const paintBrush = TestUtils.scryRenderedDOMComponentsWithClass(toolPanel, 'tool-panel-tool')[1];
+  TestUtils.Simulate.click(paintBrush);
+  const toolPanelOptions = TestUtils.findRenderedComponentWithType(toolPanel, ToolPanelOptions);
 
   it('should identify the currently selected stroke weight (with the selected class)', () => {
     const currentlySelectedLine = TestUtils.findRenderedDOMComponentWithClass(toolPanelOptions, 'line-container selected');
@@ -18,17 +22,9 @@ describe('tool panel options component', () => {
   });
 
   it('should set the stroke weight in the application state', () => {
-    const toolPanelOptions = TestUtils.findRenderedComponentWithType(app, ToolPanelOptions);
     const mediumWeightLine = TestUtils.scryRenderedDOMComponentsWithClass(toolPanelOptions, 'line-container')[2];
     TestUtils.Simulate.click(mediumWeightLine);
     expect(app.state.toolOptions.stroke).toEqual(3);
   });
-
-  it('should show line weight options if the paint brush is selected', () => {
-    const paintBrush = TestUtils.scryRenderedDOMComponentsWithClass(toolPanel, 'tool-panel-tool')[1];
-    TestUtils.Simulate.click(paintBrush);
-    const toolPanelOptions = TestUtils.scryRenderedDOMComponentsWithClass(toolPanel, 'tool-panel-options');
-    expect(toolPanelOptions).to.have.length(1)
-  })
 
 });
